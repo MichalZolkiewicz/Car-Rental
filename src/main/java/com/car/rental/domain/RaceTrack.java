794c97ccbdfa4a7ad503fad72395128768354275
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-29 22:04:44 +0200

    racetrack entity

--- src/main/java/com/car/rental/domain/RaceTrack.java
+++ src/main/java/com/car/rental/domain/RaceTrack.java
@@ -1,4 +1,28 @@
 package com.car.rental.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name = "race_track")
 public class RaceTrack {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "track_name")
+    private String trackName;
+
+    @Column(name = "track_closed")
+    private boolean isClosed;
+
 }
