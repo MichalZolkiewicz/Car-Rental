3277786d4ab474813868de76b57ae2d9dcbd5688
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-29 22:01:21 +0200

    Rental entity

--- src/main/java/com/car/rental/domain/Rental.java
+++ src/main/java/com/car/rental/domain/Rental.java
@@ -1,4 +1,41 @@
 package com.car.rental.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "rentals")
 public class Rental {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "track_id")
+    private RaceTrack raceTrack;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    @Column(name = "price")
+    private Double price;
+
 }
