0a624119bafd6cd45dfc6997b6c396a89bac510a
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-29 21:46:49 +0200

    car entity

--- src/main/java/com/car/rental/domain/Car.java
+++ src/main/java/com/car/rental/domain/Car.java
@@ -1,4 +1,38 @@
 package com.car.rental.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@Entity(name = "cars")
 public class Car {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "price")
+    private Double price;
+
+    public Car(String brand, String model, Double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
 }
