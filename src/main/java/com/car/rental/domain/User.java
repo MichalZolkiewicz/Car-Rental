afbaf2720543f27f36d91b843b84c8e35faf3d78
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-29 21:49:45 +0200

    user entity

--- src/main/java/com/car/rental/domain/User.java
+++ src/main/java/com/car/rental/domain/User.java
@@ -1,4 +1,30 @@
 package com.car.rental.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "blocked")
+    private boolean isBlocked;
 }
