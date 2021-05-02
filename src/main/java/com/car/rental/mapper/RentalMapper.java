3e1742bff36f59e79f1dcb7ebe733652e91a5184
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-02 20:10:20 +0200

    mapper for Rental created.

--- src/main/java/com/car/rental/mapper/RentalMapper.java
+++ src/main/java/com/car/rental/mapper/RentalMapper.java
@@ -1,4 +1,31 @@
 package com.car.rental.mapper;
 
+import com.car.rental.domain.Rental;
+import com.car.rental.domain.RentalDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class RentalMapper {
+
+    public Rental mapToRental(RentalDto rentalDto) {
+        return new Rental(rentalDto.getId(), rentalDto.getStartDate(), rentalDto.getEndDate(), rentalDto.getPrice());
+    }
+
+    public RentalDto mapToRentalDto(Rental rental) {
+        return RentalDto.builder()
+                .id(rental.getId())
+                .startDate(rental.getStartDate())
+                .endDate(rental.getReturnDate())
+                .price(rental.getPrice())
+                .build();
+    }
+
+    public List<RentalDto> mapToRentalDtoList(List<Rental> rentalList) {
+        return rentalList.stream()
+                .map(this::mapToRentalDto)
+                .collect(Collectors.toList());
+    }
 }
