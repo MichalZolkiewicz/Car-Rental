e2ab3f2df4ac227d95a47c98d8675fb0b0809ea0
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-02 23:12:40 +0200

    RaceTrack dto class created

--- src/main/java/com/car/rental/domain/RaceTrackDto.java
+++ src/main/java/com/car/rental/domain/RaceTrackDto.java
@@ -1,4 +1,15 @@
 package com.car.rental.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Builder
+@Getter
 public class RaceTrackDto {
+
+    private final Long id;
+    private final String trackName;
+    private final boolean isClosed;
 }
