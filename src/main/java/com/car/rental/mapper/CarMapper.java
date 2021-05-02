b138375716b390248a74ef84ebc1a1cd502be835
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-02 20:06:35 +0200

    mapping to Car, CarDto and List<CarDto> methods created.

--- src/main/java/com/car/rental/mapper/CarMapper.java
+++ src/main/java/com/car/rental/mapper/CarMapper.java
@@ -1,4 +1,33 @@
 package com.car.rental.mapper;
 
+import com.car.rental.domain.Car;
+import com.car.rental.domain.CarDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class CarMapper {
+
+    public Car mapToCar(CarDto carDto) {
+        return new Car(carDto.getBrand(), carDto.getModel(), carDto.getPrice());
+    }
+
+    public CarDto mapToCarDto(Car car) {
+
+        return CarDto.builder()
+                .id(car.getId())
+                .brand(car.getBrand())
+                .model(car.getModel())
+                .price(car.getPrice())
+                .status(car.getStatus())
+                .build();
+    }
+
+    public List<CarDto> mapToCarDtoList(List<Car> carList) {
+        return carList.stream()
+                .map(this::mapToCarDto)
+                .collect(Collectors.toList());
+    }
 }
