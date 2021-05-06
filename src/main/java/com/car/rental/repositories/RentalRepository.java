d08fa79c6e92c3488b7b2075e0358a5af5412970
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-06 22:42:59 +0200

    methods adjusted

--- src/main/java/com/car/rental/repositories/RentalRepository.java
+++ src/main/java/com/car/rental/repositories/RentalRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @Repository
 public interface RentalRepository extends CrudRepository<Rental, Long> {
+
+    List<Rental> findAll();
+
+    Rental findById(long id);
 }
