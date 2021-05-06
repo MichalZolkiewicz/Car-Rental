ed5426a1c60ce60a56d98511126c4a8d7815aa8b
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-06 22:43:26 +0200

    basic crud endpoint created

--- src/main/java/com/car/rental/controller/RentalController.java
+++ src/main/java/com/car/rental/controller/RentalController.java
@@ -1,4 +1,42 @@
 package com.car.rental.controller;
 
+import com.car.rental.domain.RentalDto;
+import com.car.rental.facade.RentalFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
 public class RentalController {
+
+    private final RentalFacade rentalFacade;
+
+    @GetMapping(value = "/rentals")
+    public List<RentalDto> getAllRentals() {
+        return rentalFacade.getAllRental();
+    }
+
+    @GetMapping(value = "/rentals/{rentalId}")
+    public RentalDto getRental(@PathVariable Long rentalId) {
+        return rentalFacade.getRentalDto(rentalId);
+    }
+
+    @PostMapping(value = "/rentals")
+    public Long createRental(@RequestBody RentalDto rentalDto) {
+        return rentalFacade.saveRental(rentalDto);
+    }
+
+    @PutMapping(value = "rentals")
+    public RentalDto updateRental(@RequestBody RentalDto rentalDto) {
+        return rentalFacade.updateRental(rentalDto);
+    }
+
+    @DeleteMapping(value = "rentals/{rentalId}")
+    public void deleteRental(@PathVariable Long rentalId) {
+        rentalFacade.deleteRental(rentalId);
+    }
 }
