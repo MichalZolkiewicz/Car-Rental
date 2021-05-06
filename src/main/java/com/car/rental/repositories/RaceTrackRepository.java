e761b2562f083783252a387f01985c8a94cb6ce4
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-06 23:56:08 +0200

    methods adjusted

--- src/main/java/com/car/rental/repositories/RaceTrackRepository.java
+++ src/main/java/com/car/rental/repositories/RaceTrackRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @Repository
 public interface RaceTrackRepository extends CrudRepository<RaceTrack, Long> {
+
+    List<RaceTrack> findAll();
+
+    RaceTrack findById(final long id);
 }
