0b4ed4c48004fbb39ae36592ac6d4e57b8ec5ddf
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-05-18 23:32:54 +0200

    EmailService and EmailScheduler added

---
+++ src/main/java/com/car/rental/domain/Mail.java
@@ -0,0 +1,16 @@
+package com.car.rental.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class Mail {
+
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCC;
+}
---
+++ src/main/java/com/car/rental/scheduler/EmailScheduler.java
@@ -0,0 +1,35 @@
+package com.car.rental.scheduler;
+
+import com.car.rental.config.AdminConfig;
+import com.car.rental.domain.Mail;
+import com.car.rental.domain.Rental;
+import com.car.rental.repositories.RentalRepository;
+import com.car.rental.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Rental is expiring soon!";
+    private final EmailService emailService;
+    private final RentalRepository rentalRepository;
+    private final AdminConfig adminConfig;
+
+    @Scheduled(cron = "0 */6 * * *")
+    public void sendSoonExpiringRentalInformation() {
+
+        List<Rental> rentalList = rentalRepository.findAll();
+        for (Rental rental : rentalList) {
+            LocalDate returnDate = rental.getReturnDate();
+            if (returnDate == LocalDate.now().plusDays(1)) {
+                emailService.send(new Mail(rental.getUser().getEmail(), SUBJECT, "Your rental is expiring tomorrow", adminConfig.getAdminMail()));
+            }
+        }
+    }
+}
